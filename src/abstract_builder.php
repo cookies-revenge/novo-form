a303e4c0afea3b6bc6d69a0a74b0154f1e8d9445
ps_markom <milunovic@outlook.com>
2019-12-17 09:16:24 +0000

    ...

--- src/abstract_builder.php
+++ src/abstract_builder.php
@@ -5,8 +5,7 @@ namespace CookiesRevenge\Novo\Utilities\NovoFormBuilder;
 abstract class Abstract_Builder
 {
 
-    abstract public function BuildTable();
-    abstract public function BuildTableResults();
+    abstract public function BuildForm();
 
     public function SetTemplatingEngine($templatingEngine)
     {
@@ -47,109 +46,6 @@ abstract class Abstract_Builder
         return $this;
     }
 
-    /**
-     * Define active page for the table.
-     * This is used when user selects a pagination page.
-     *
-     * @param int $resultsPerPage new value
-     * @return $this The current object (for fluent API support)
-     */
-    public function SetActivePage($activePage)
-    {
-        $this->activePage_ = $activePage;
-        return $this;
-    }
-
-    /**
-     * Define number of table rows to be displayed in a single page.
-     * Default value = 20. You may use this function to override it.
-     *
-     * @param int $resultsPerPage new value
-     * @return $this The current object (for fluent API support)
-     */
-    public function SetResultsPerPage($resultsPerPage)
-    {
-        $this->resultsPerPage_ = $resultsPerPage;
-        return $this;
-    }
-
-    /**
-     * Define sorting column for the table.
-     * This refers to a column name from definitions map.
-     *
-     * @param string $sortingColumn new value
-     * @return $this The current object (for fluent API support)
-     */
-    public function SetSortingColumn($sortingColumn)
-    {
-        $this->sortingColumn_ = $sortingColumn;
-        return $this;
-    }
-
-    /**
-     * Define sorting order for the sorting column. Allowed values "ASC" or "DESC"
-     *
-     * @param string $sortingOrder new value
-     * @return $this The current object (for fluent API support)
-     */
-    public function SetSortingOrder($sortingOrder)
-    {
-        if (!in_array(strtoupper($sortingOrder), [Table_Builder_Constants::ORDER_ASCENDING, Table_Builder_Constants::ORDER_DESCENDING])) {
-            throw new \Exception("Invalid value set for Sorting Type");
-        }
-
-        $this->sortingOrder_ = $sortingOrder;
-        return $this;
-    }
-
-    /**
-     * Define whether Table should display all results in one page (no pagination)
-     *
-     * @param boolean $showAll new value
-     * @return $this The current object (for fluent API support)
-     */
-    public function SetShowAll(bool $showAll)
-    {
-        $this->showAll_ = $showAll;
-        return $this;
-    }
-
-    /**
-     * Define whether Table should be responsive.
-     *
-     * @param boolean $isResponsive new value
-     * @return $this The current object (for fluent API support)
-     */
-    public function SetIsResponsive(bool $isResponsive)
-    {
-        $this->isResponsive_ = $isResponsive;
-        return $this;
-    }
-
-    /**
-     * Define whether Table should be striped.
-     *
-     * @param boolean $isStriped new value
-     * @return $this The current object (for fluent API support)
-     */
-    public function SetIsStriped(bool $isStriped)
-    {
-        $this->isStriped_ = $isStriped;
-        return $this;
-    }
-
-    /**
-     * Define whether Table should be bordered.
-     *
-     * @param boolean $isBordered new value
-     * @return $this The current object (for fluent API support)
-     */
-    public function SetIsBordered(bool $isBordered)
-    {
-        $this->isBordered_ = $isBordered;
-        return $this;
-    }
-
     /*
      * templating engine
      */
@@ -167,37 +63,4 @@ abstract class Abstract_Builder
      */
     /** @var array */
     protected $dataCollection_ = [];
-
-    /*
-     * dataset sorting and batch options
-     */
-    /** @var integer */
-    protected $activePage_ = 1;
-
-    /** @var integer */
-    protected $resultsPerPage_ = 20;
-
-    /** @var integer */
-    protected $resultsTotal_ = 0;
-
-    /** @var string */
-    protected $sortingColumn_ = "Id";
-
-    /** @var string */
-    protected $sortingOrder_ = "ASC";
-
-    /** @var boolean */
-    protected $showAll_ = false;
-
-    /*
-     * styling options
-     */
-    /** @var boolean */
-    protected $isBordered_;
-
-    /** @var boolean */
-    protected $isStriped_;
-
-    /** @var boolean */
-    protected $isResponsive_;
 }
